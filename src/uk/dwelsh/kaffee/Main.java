7415ac098f7602e2239ff31f56509577f51441d5
Daniel Welsh <danjwelsh24@gmail.com>
2019-12-02 00:18:54 +0100

    :sparkles: uploading on keyboard input

--- src/uk/dwelsh/kaffee/Main.java
+++ src/uk/dwelsh/kaffee/Main.java
@@ -6,31 +6,38 @@ import uk.dwelsh.kaffee.models.User;
 import uk.dwelsh.kaffee.store.RfidStore;
 import uk.dwelsh.kaffee.store.UserStore;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Logger logger = new Logger("log.txt");
-
         RfidStore store = new RfidStore(String.format("%s/rfid_store.json", args[1]));
-        RfidTag rfid = store.get(t -> t.getTag().equals(args[0]));
-
-        logger.log(String.format("Scanned: %s - Status: %s", args[0], rfid == null ? "UNKNOWN" : "MATCH"));
-
-        if (rfid == null) {
-            return;
-        }
-
         UserStore userStore = new UserStore(String.format("%s/user_store.json", args[1]));
-        User user = userStore.get(u -> u.getEmail().equals(rfid.getUserId()));
 
-        System.out.println(user);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine().trim();
 
-        KaffeeClient api = new KaffeeClient();
-        User oldUser = user;
-        user.setSession(api.login(user));
-        userStore.update(oldUser, user);
+            RfidTag rfid = store.get(t -> t.getTag().equals(input));
 
-        api.LogCoffee(user.getSession().getToken(), rfid.getCoffee());
-        logger.log(String.format("Logged an %s for %s", rfid.getCoffee(), rfid.getUserId()));
+            logger.log(String.format("Scanned: %s - Status: %s", args[0], rfid == null ? "UNKNOWN" : "MATCH"));
+
+            if (rfid == null) {
+                return;
+            }
+
+            User user = userStore.get(u -> u.getEmail().equals(rfid.getUserId()));
+
+            System.out.println(user);
+
+            KaffeeClient api = new KaffeeClient();
+            User oldUser = user;
+            user.setSession(api.login(user));
+            userStore.update(oldUser, user);
+
+            api.LogCoffee(user.getSession().getToken(), rfid.getCoffee());
+            logger.log(String.format("Logged an %s for %s", rfid.getCoffee(), rfid.getUserId()));
+        }
     }
 }
