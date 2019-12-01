a508f329cb40b3b68d5a8cf40f899119baed90c3
Daniel Welsh <danjwelsh24@gmail.com>
2019-12-01 23:24:23 +0100

    :wrench: adding ant build

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-mkdir -p libs
-wget -P ./libs/ https://repo1.maven.org/maven2/com/google/code/gson/gson/2.8.2/gson-2.8.2.jar
-wget -P ./libs/ https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib/1.3.61/kotlin-stdlib-1.3.61.jar
-wget -P ./libs/ https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib-common/1.3.61/kotlin-stdlib-common-1.3.61.jar
-wget -P ./libs/ https://repo1.maven.org/maven2/com/squareup/okhttp3/okhttp/4.2.2/okhttp-4.2.2.jar
-wget -P ./libs/ https://repo1.maven.org/maven2/com/squareup/okio/okio/2.4.1/okio-2.4.1.jar
+mkdir -p lib
+wget -P ./lib/ https://repo1.maven.org/maven2/com/google/code/gson/gson/2.8.2/gson-2.8.2.jar
+wget -P ./lib/ https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib/1.3.61/kotlin-stdlib-1.3.61.jar
+wget -P ./lib/ https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib-common/1.3.61/kotlin-stdlib-common-1.3.61.jar
+wget -P ./lib/ https://repo1.maven.org/maven2/com/squareup/okhttp3/okhttp/4.1.0/okhttp-4.1.0.jar
+wget -P ./lib/ https://repo1.maven.org/maven2/com/squareup/okio/okio/2.4.1/okio-2.4.1.jar
