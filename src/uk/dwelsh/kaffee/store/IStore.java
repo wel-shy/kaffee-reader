4d3935f2c48e63ee550219761434d48f5bcdb8ad
Daniel Welsh <danjwelsh24@gmail.com>
2019-12-08 19:16:32 +0100

    :fire: removing java implementation

--- src/uk/dwelsh/kaffee/store/IStore.java
+++
@@ -1,9 +0,0 @@
-package uk.dwelsh.kaffee.store;
-
-import java.io.FileNotFoundException;
-import java.util.function.Predicate;
-
-public interface IStore<T> {
-    void initialiseStore() throws FileNotFoundException;
-    T get(Predicate<? super T> searchLambda);
-}
